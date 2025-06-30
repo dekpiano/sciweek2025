942b58fd1186da2df7f3f44c3bf9ac5de015b5d4
dekpiano <dekpiano4815@gmail.com>
2025-06-30 17:48:20 +0700

    V1

---
+++ app/Views/User/Pages/Home.php
@@ -0,0 +1,206 @@
+<?= $this->extend('User/layouts/main') ?>
+<?= $this->section('title') ?>
+หน้าแรก
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<div class="bg-primary hero-header">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6 text-center text-lg-start">
+                <h1 class="text-white mb-4 animated zoomIn" style="font-size: 3.7rem;">SCIENCE WEEK 2025</h1>
+                <h2 class="text-white pb-3 animated zoomIn">งานมหกรรมวันวิทยาศาสตร์ <br> “วิทย์ยุคใหม่ AI เปลี่ยนโลก”
+                </h2>
+                <a href="" class="btn btn-outline-light rounded-pill border-2 py-3 px-3 animated slideInRight">6 - 7
+                    สิงหาคม 2568</a>
+                    <a target="_blank" href="https://maps.app.goo.gl/NPBV39wtktEs6nXX6" class="btn btn-light rounded-pill border-2 py-3 px-3 animated slideInRight"> <i class="bi bi-geo-alt-fill"></i> Google Map</a>
+            </div>
+            <div class="col-lg-6 text-center text-lg-start">
+                <img class="img-fluid animated zoomIn" src="<?=base_url()?>public/img/home/logo_sci.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Navbar & Hero End -->
+
+<!-- Service Start -->
+<div class="py-6" id="service">
+    <div class="container">
+        <div class="mx-auto text-center wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <div class="d-inline-block border rounded-pill text-primary px-4 mb-3">Our Services</div>
+            <h2 class="mb-5">รายการแข่งขัน</h2>
+        </div>
+        <div class="row g-4">
+            <?php foreach ($ListCompetition as $competition): ?>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded h-100">
+                    <div class="d-flex justify-content-between">
+                        <div class="service-icon">
+                            <i class="<?=$competition->ctype_icon?> h1 text-white"></i>
+                        </div>
+                        <!-- <a class="service-btn" href="">
+                            <i class="bi bi-link-45deg h1 text-white"></i>
+                        </a> -->
+                    </div>
+                    <div class="p-5">
+                        <h5 class="mb-3">การแข่งขัน <?=$competition->ctype_name?></h5>
+                        <span ><?=$competition->ctype_detail?></span>
+                        <div class="mt-4">
+                            <a href="" class="btn-info btn"><i class="bi bi-file-earmark-fill"></i> รายละเอียด</a>
+                            <a target="_blank" href="<?=$competition->ctype_link_reg?>" class="btn-success btn"><i class="bi bi-pencil-square"></i> สมัครแข่งขัน</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+<!-- Newsletter Start -->
+<div class="bg-primary my-6 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container px-lg-5">
+        <div class="row align-items-center" style="height: 250px;">
+            <div class="col-12 col-md-6">
+                <h3 class="text-white">AI, วิทยาศาสตร์, และเทคโนโลยี: ความสัมพันธ์ที่ขับเคลื่อนอนาคต</h3>
+                <p class="text-white">วิทยาศาสตร์ค้นพบความรู้ใหม่ ๆ ส่วนเทคโนโลยีนำความรู้นั้นไปใช้ประโยชน์ และ AI ก็เป็นผลผลิตสำคัญจากการผสานรวมกันของวิทยาศาสตร์และเทคโนโลยี ที่กำลังเปลี่ยนโลกของเราอย่างรวดเร็ว</p>
+            </div>
+            <div class="col-md-6 text-center mb-n5 d-none d-md-block">
+                <img class="img-fluid " style="max-height: 400px;margin-top:-88px;"
+                    src="<?=base_url()?>public/img/home/logo_sci2.png">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Newsletter End -->
+
+
+<!-- About Start -->
+<div class="py-6">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6 wow zoomIn" data-wow-delay="0.1s">
+                <img class="img-fluid" src="<?=base_url()?>public/img/home/logo_sci3.png" alt="Logo">
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="d-inline-block border rounded-pill text-primary px-4 mb-3">About Us</div>
+                <h2 class="mb-4">งานนี้เหมาะกับใคร?</h2>
+                <div class="row g-3 mb-4">
+                    <div class="col-12 d-flex">
+                        <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                            <i class="bi bi-1-square text-white"></i>
+                        </div>
+                        <div class="ms-4">
+                            <h6>นักเรียนและนักศึกษา</h6>
+                            <span>
+                                เป็นโอกาสดีในการเรียนรู้วิทยาศาสตร์และเทคโนโลยีผ่านกิจกรรมต่าง ๆ
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex">
+                        <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
+                            <i class="bi bi-2-square text-white"></i>
+                        </div>
+                        <div class="ms-4">
+                            <h6>บุคลากรทางการศึกษา</h6>
+                            <span>สามารถเข้าร่วมเพื่อเรียนรู้นวัตกรรมและแนวทางการใช้เทคโนโลยีในกระบวนการเรียนการสอน</span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+
+
+
+
+
+<!-- Client Start -->
+<!-- <div class="bg-primary my-6 py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="owl-carousel client-carousel">
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-1.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-2.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-3.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-4.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-5.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-6.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-7.png" alt=""></a>
+            <a href="#"><img class="img-fluid" src="<?=base_url()?>public/img/logo-8.png" alt=""></a>
+        </div>
+    </div>
+</div> -->
+<!-- Client End -->
+
+
+<!-- Testimonial Start -->
+<!-- <div class="py-6">
+    <div class="container">
+        <div class="mx-auto text-center wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <div class="d-inline-block border rounded-pill text-primary px-4 mb-3">Testimonial</div>
+            <h2 class="mb-5">What Our Clients Say!</h2>
+        </div>
+        <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+            <div class="testimonial-item rounded p-4">
+                <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded-circle"
+                        src="<?=base_url()?>public/img/testimonial-1.jpg">
+                    <div class="ps-3">
+                        <h6 class="mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-item rounded p-4">
+                <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded-circle"
+                        src="<?=base_url()?>public/img/testimonial-2.jpg">
+                    <div class="ps-3">
+                        <h6 class="mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-item rounded p-4">
+                <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded-circle"
+                        src="<?=base_url()?>public/img/testimonial-3.jpg">
+                    <div class="ps-3">
+                        <h6 class="mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-item rounded p-4">
+                <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded-circle"
+                        src="<?=base_url()?>public/img/testimonial-4.jpg">
+                    <div class="ps-3">
+                        <h6 class="mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+<!-- Testimonial End -->
+
+
+
+<?= $this->endSection() ?>
---
+++ app/Views/User/layouts/main.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title><?= $this->renderSection('title') ?> | สัปดาห์วิทยาศาสตร์</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="<?=base_url()?>public/img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+    
+
+    <!-- Libraries Stylesheet -->
+    <link href="<?=base_url()?>public/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="<?=base_url()?>public/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="<?=base_url()?>public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="<?=base_url()?>public/css/style.css?v=1" rel="stylesheet">
+    <style>
+        *{
+            font-family: 'K2D', sans-serif !important;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="index.html" class="navbar-brand p-0">
+                    <h1 class="m-0">สัปดาห์วิทยาศาสตร์ 2025</h1>
+                    <!-- <img src="<?=base_url()?>public/img/logo.png" alt="Logo"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <i class="bi bi-layout-text-sidebar"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="#service" class="nav-item nav-link">รายการแข่งขัน</a>
+                        <a href="about.html" class="nav-item nav-link">กำหนดการแข่งขัน</a>
+                        <a href="service.html" class="nav-item nav-link">ตารางแข่งขัน</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">ดาวน์โหลดเอกสาร</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="feature.html" class="dropdown-item">หนังสือโครงการ</a>
+                                <a href="quote.html" class="dropdown-item">หนังสือเชิญ</a>
+                                <a href="team.html" class="dropdown-item">กติกาการแข่งขัน</a>
+                                <a href="testimonial.html" class="dropdown-item">แผนผังการจัดงาน</a>
+                                <a href="404.html" class="dropdown-item">รายชื่อโรงแรม</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link">ผลการการแข่งขัน</a>
+                        <a href="contact.html" class="nav-item nav-link">รายชื่อทีมสมัครแข่งขัน</a>
+                    </div>
+                    <!-- <a href="#service" class="btn btn-light rounded-pill text-primary py-2 px-4 ms-lg-5">สมัครแข่งขัน</a> -->
+                </div>
+            </nav>
+
+
+        </div>
+
+        <!-- Main Content -->
+        <main>
+            <?= $this->renderSection('content') ?>
+        </main>
+
+
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s"
+            style="margin-top: 6rem;">
+            <div class="container py-5">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4">สัปดาห์วิทยาศาสตร์ 2568</h5>
+                        <p><i class="bi bi-geo-alt"></i> 160 ม.1 ต.นครสวรรค์ออก อ.เมือง จ.นครสวรรค์ 60000</p>
+                        
+                        <p><i class="bi bi-telephone"></i> 056-009-667</p>
+                        <p><i class="bi bi-envelope"></i> skjns160@skj.ac.th</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/SKJNS160"><i class="bi bi-facebook"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="https://www.youtube.com/channel/UC7p4cQQuIFLyrF68p7JbWDw"><i class="bi bi-youtube"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 class="text-white mb-4"></h5>
+                        <a class="btn btn-link" href="">รายการแข่งขัน</a>
+                        <a class="btn btn-link" href="">กำหนดการแข่งขัน</a>
+                        <a class="btn btn-link" href="">ตารางแข่ง</a>
+                        <a class="btn btn-link" href="">ดาวน์โหลดเอกสาร</a>
+                        <a class="btn btn-link" href="">ผลการการแข่งขัน</a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <h4 class="text-white mb-4">กลุ่มสาระการเรียนรู้วิทยาศาสตร์และเทคโนโลยี</h5>
+                        <p><i class="bi bi-geo"></i> โรงเรียนสวนกุหลาบวิทยาลัย (จิรประวัติ) นครสวรรค์</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">SciWeek2025</a>, All Right Reserved.
+
+                            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                            Author By <a class="border-bottom" href="https://www.facebook.com/dekpiano">Dekpiano</a> 
+                          
+                        </div>
+                        <!-- <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="">Home</a>
+                                <a href="">Cookies</a>
+                                <a href="">Help</a>
+                                <a href="">FQAs</a>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url()?>public/lib/wow/wow.min.js"></script>
+    <script src="<?=base_url()?>public/lib/easing/easing.min.js"></script>
+    <script src="<?=base_url()?>public/lib/waypoints/waypoints.min.js"></script>
+    <script src="<?=base_url()?>public/lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="<?=base_url()?>public/js/main.js"></script>
+</body>
+
+</html>
